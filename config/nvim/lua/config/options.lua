6ec4cadb804e42b03dccfa4ecae0a9d9c227ac38
Daniel Correia <daniel.correia@marks-and-spencer.com>
2025-02-06 10:13:10 +0000

    better organization

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -1,10 +1,3 @@
--- Set <space> as the leader key
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
-
--- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = true
-
 -- [[ Setting options ]]
 
 -- Make line numbers default
