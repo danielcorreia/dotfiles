07c1dad906292a59231e45939b3cd2a1de7945a8
Daniel Correia <daniel.correia@marks-and-spencer.com>
2025-02-13 12:50:24 +0000

    gitsigns: use default signs, blame shortcuts

--- config/nvim/lua/plugins/gitsigns.lua
+++ config/nvim/lua/plugins/gitsigns.lua
@@ -2,12 +2,17 @@
 return {
 	"lewis6991/gitsigns.nvim",
 	opts = {
-		signs = {
-			add = { text = "+" },
-			change = { text = "~" },
-			delete = { text = "_" },
-			topdelete = { text = "‾" },
-			changedelete = { text = "~" },
-		},
+		on_attach = function(bufnr)
+			local gitsigns = require("gitsigns")
+
+			local function map(mode, l, r, opts)
+				opts = opts or {}
+				opts.buffer = bufnr
+				vim.keymap.set(mode, l, r, opts)
+			end
+
+			map("n", "<leader>B", gitsigns.blame, { desc = "Display [B]lame" })
+			map("n", "<leader>b", gitsigns.toggle_current_line_blame, { desc = "Toggle current line [b]lame" })
+		end,
 	},
 }
